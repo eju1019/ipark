9352a2659893b7f29e2a3f67f577d006598bf3bb
HomonymB <154942133+HomonymB@users.noreply.github.com>
2024-02-07 12:05:19 +0900

    Update Arum.py

--- Arum.py
+++ Arum.py
@@ -11,6 +11,8 @@ df.head(3)
 df.info()
 
 # 딕셔너리 모양 문자열 칼럼 데이터프레임으로 만들기  
+# 딕셔너리 모양 문자열 칼럼 데이터프레임으로 만들기 
+
 def get_columns(columns_list: list):
     """" 딕셔너리 모양으로 들어간 문자열 칼럼을 데이터 프레임으로 변환해주는 함수  
     Args:
@@ -18,14 +20,15 @@ def get_columns(columns_list: list):
 
     Returns:
         pd.DataFrame: 데이터프레임
-
     """""
+    df2 = df.copy()
     for col in columns_list:
         chagne_df = pd.DataFrame(json.loads(row) for row in list(df[col]))
         chagne_df.columns = [f'{col}.{subcol}' for subcol in chagne_df.columns]
-        df2 = df.drop(col, axis=1)
+        df2 = df2.drop(col, axis=1)
         df2 = pd.concat([df2, chagne_df], axis = 1)
     return df2
 
 json_columns = ['device', 'geoNetwork', 'totals', 'trafficSource']
 get_columns(json_columns)
+
